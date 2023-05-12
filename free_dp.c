87d23cca2a039864a586dbbd91afd8f2b9204193
Junenancy <irokajuliet01@gmail.com>
2023-05-12 02:57:39 -0700

    shell_project

---
+++ free_dp.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+/**
+ * free_dp - free all file
+ * @line: double pointer to free
+ *
+ * Return: ptr null
+ */
+
+char **free_dp(char **line)
+{
+	int i;
+
+	for (i = 0; line[i]; x++)
+	{
+		free(line[i]);
+		line[i] = NULL;
+	}
+	free(line);
+	line = NULL;
+	return (NULL);
+}
