3163923e4b3a18c7acda8601d83358a7205456ff
Jasmine <jasmineenams@gmail.com>
2023-05-15 00:32:43 -0700

    prompt file

---
+++ prompt.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+/**
+ * prompt_shell - function that prints the prompt
+ *
+ * @void: empty
+ * Return: nothing
+ */
+void prompt_shell(void)
+{
+	write(STDOUT, "\033[1;31mMi_shell/user@root$ ", 32);
+	write(STDOUT, "\033[0m", 4);
+}
